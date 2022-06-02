efaaa46562dd1d2de5939dbf1e6c7d214598bf78
kuro <k537056@kansai-u.ac.jp>
2022-06-02 15:32:08 +0900

    更新

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def distance(x1, x2, y1, y2):
 
 def Setting(FILENAME):
     mat = []
-    with open('/home/kurozumi/デスクトップ/benchmark/' + FILENAME, 'r', encoding='utf-8') as fin:
+    with open('/home/kurozumi/デスクトップ/benchmark2/' + FILENAME, 'r', encoding='utf-8') as fin:
         for line in fin.readlines():
             row = []
             toks = line.split()
@@ -54,97 +54,106 @@ def Setting(FILENAME):
 
     return Setting_Info, request_number, depo_zahyo, c, e, l, noriori
 
-if __name__ =='__main__':
-    FILENAME = 'darp01_ex.txt'
-    Setting_Info =Setting(FILENAME)
-    Setting_Info_base =Setting_Info[0]
 
-    T = int(Setting_Info_base[5])    #時間数
-    n= int(Setting_Info[1])+1 #デポを含めた頂点数
-    Request = int((n-1)/2)  #リクエスト数
-    Distance = Setting_Info[3]  #距離
-    e = Setting_Info[4] #early time
-    l=Setting_Info[5]   #delay time
+if __name__ == '__main__':
+    FILENAME = 'darp_ex3.txt'
+    Setting_Info = Setting(FILENAME)
+    Setting_Info_base = Setting_Info[0]
+
+    T = int(Setting_Info_base[5])  # 時間数
+    n = int(Setting_Info[1]) + 1  # デポを含めた頂点数
+    Request = int((n - 1) / 2)  # リクエスト数
+    Distance = Setting_Info[3]  # 距離
+    e = Setting_Info[4]  # early time
+    l = Setting_Info[5]  # delay time
 
     noriori = Setting_Info[6]
 
+    Time_expand = 1
 
-    G = nx.DiGraph()  #ノード作成
+    G = nx.Graph()  # ノード作成
     for i in range(n):
-        early_time = e[i]+1
-        late_time = l[i]+1
-        if e[i] ==0:
-            early_time =0
-        add_node = range(early_time,late_time)
-        for j in add_node:
-            G.add_node((i,j))
-
-    #G.add_edge((0,0),(1,5),weight=Setting_Info[3][0][1])
-
-
-    for a in range(n):
-        early_time = e[a]+1
-        late_time = l[a]+1
-        if e[a]==0:
-            early_time=0
+        early_time = e[i]
+        late_time = l[i]
 
         add_node = range(early_time, late_time)
         for j in add_node:
-            for i in range(n-1): #各ノードからdepoに帰るエッジがつくられていない & ここのループだとdepoのノード同士がつながらないので改善が必要
-                if a==0 and noriori[i+1] >0:
-                    next_early_time = e[i+1]+1
-                    next_late_time = l[i+1]+1
-                    if e[i+1]==0:
-                        next_early_time=0
-                    next_add_node = range(next_early_time,next_late_time)
-                    for k in next_add_node:
-                        distance_check = math.ceil(Distance[a][i+1])
-                        if distance_check+j <= k:       #このedgeを追加するコードは無駄な処理を含んでいます。直す必要アリ(5/10)
-                            if a==i+1:
-                                if k-j ==1 :
-                                    G.add_edge((a, j), (i+1, k), weight=Distance[a][i+1])
-                            else:
-                                G.add_edge((a, j), (i+1, k), weight=Distance[a][i+1])
-                elif not a==0:
-                    next_early_time = e[i + 1] + 1
-                    next_late_time = l[i + 1] + 1
-                    if e[i + 1] == 0:
-                        next_early_time = 0
-                    next_add_node = range(next_early_time, next_late_time)
-                    for k in next_add_node:
-                        distance_check = math.ceil(Distance[a][i + 1])
-                        if distance_check + j <= k:  # このedgeを追加するコードは無駄な処理を含んでいます。直す必要アリ(5/10)
-                            if a == i + 1:
-                                if k - j == 1:
-                                    G.add_edge((a, j), (i + 1, k), weight=Distance[a][i + 1])
-                            else:
-                                G.add_edge((a, j), (i + 1, k), weight=Distance[a][i + 1])
+            if j % Time_expand == 0:
+                G.add_node((i, j))
 
-    """
-    
-    """
+    # G.add_edge((0,0),(1,5),weight=Setting_Info[3][0][1])
+
+    for a in range(n):
+        early_time = e[a]
+        late_time = l[a]
+
+        add_node = range(early_time, late_time)
+        for j in add_node:
+            if j % Time_expand == 0:
+                b = 0
+                for i in range(n - 1):  # 各ノードからdepoに帰るエッジがつくられていない & ここのループだとdepoのノード同士がつながらないので改善が必要
+                    if a == 0 and noriori[i + 1] > 0:
+                        next_early_time = e[i + 1]
+                        next_late_time = l[i + 1]
+
+                        next_add_node = range(next_early_time, next_late_time)
+                        for k in next_add_node:
+                            if k % Time_expand == 0:
+                                distance_check = math.ceil(Distance[a][i + 1])
+                                if distance_check + j <= k:  # このedgeを追加するコードは無駄な処理を含んでいます。直す必要アリ(5/10)
+                                    b = 1
+                                    if a == i + 1:
+                                        if k - j == 1:
+                                            G.add_edge((a, j), (i + 1, k), weight=Distance[a][i + 1])
+                                    else:
+                                        G.add_edge((a, j), (i + 1, k), weight=Distance[a][i + 1])
+                                if b == 1:
+                                    break
+                    elif not a == 0:
+                        next_early_time = e[i + 1]
+                        next_late_time = l[i + 1]
+
+                        next_add_node = range(next_early_time, next_late_time)
+                        for k in next_add_node:
+                            if k % Time_expand == 0:
+                                distance_check = math.ceil(Distance[a][i + 1])
+                                if distance_check + j <= k:  # このedgeを追加するコードは無駄な処理を含んでいます。直す必要アリ(5/10)
+                                    b = 1
+                                    if a == i + 1:
+                                        if k - j == 1:
+                                            G.add_edge((a, j), (i + 1, k), weight=Distance[a][i + 1])
+                                    else:
+                                        G.add_edge((a, j), (i + 1, k), weight=Distance[a][i + 1])
+                                if b == 1:
+                                    break
+
     for i in range(n - 1):
-        if noriori[i+1] <0:
-            early_time = e[i + 1] + 1
-            late_time = l[i + 1] + 1
-            if e[i + 1] == 0:
-                early_time = 0
+        if noriori[i + 1] < 0:
+            early_time = e[i + 1]
+            late_time = l[i + 1]
+
             add_node = range(early_time, late_time)
             for j in add_node:
-                depo_repeat = range(early_time, l[0] + 1)
-                for k in depo_repeat:
-                    distance_check = math.ceil(Distance[i + 1][0])
-                    if j + distance_check <= k:
-                        G.add_edge((i + 1, j), (0, k), weight=Distance[i + 1][0])
-    loop =0
-    for i in range(T):
-        G.add_edge((0, loop), (0, i+1), weight=0)
-        loop +=1
+                if j % Time_expand == 0:
+                    b = 0
+                    depo_repeat = range(early_time, l[0])
+                    for k in depo_repeat:
+                        if k % Time_expand == 0:
+                            distance_check = math.ceil(Distance[i + 1][0])
+                            if j + distance_check <= k:
+                                b = 1
+                                G.add_edge((i + 1, j), (0, k), weight=Distance[i + 1][0])
+                            if b == 1:
+                                break
 
-    pos = {n: (n[1], -n[0]) for n in G.nodes()} #ノードの座標に注意：X座標がノード番号、Y座標が時刻t
-    #print(pos)
-    #print(G.nodes())
+    pos = {n: (n[1], -n[0]) for n in G.nodes()}  # ノードの座標に注意：X座標がノード番号、Y座標が時刻t
+    # print(pos)
+    # print(G.nodes())
     """
+     loop = 0
+    for i in range(T):
+        G.add_edge((0, loop), (0, i + 1), weight=0)
+        loop += 1
     e=5
     l=9
     L=list(range(e,l+1,1))
@@ -152,16 +161,18 @@ if __name__ =='__main__':
         G.remove_node((4,i))
     nx.draw_networkx_nodes(G, pos, node_size=10, alpha=1, node_color='blue')
     nx.draw_networkx_edges(G, pos, width=1)
-
     plt.show()
+    """
+    print(FILENAME)
+    print(Time_expand)
+    print(nx.number_of_edges(G))
+    print(nx.number_of_nodes(G))
     nx.draw_networkx_nodes(G, pos, node_size=10, alpha=1, node_color='blue')
     nx.draw_networkx_edges(G, pos, width=1)
     plt.show()
-    """
 
-    print(nx.number_of_edges(G))
-    print(nx.number_of_nodes(G))
-    #print(G.edges())
+    # print(G.edges())
+
 
 
 
